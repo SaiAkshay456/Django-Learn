6030aee0f4b0770258b10252e025cda712aa4bb0
SaiAkshay456 <saiakshayorg@gmail.com>
2025-05-19 02:28:07 +0530

    learnt how to create models and migrate

--- myfirstapp/models.py
+++ myfirstapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    first_name=models.CharField(max_length=20)
+    last_name=models.CharField(max_length=20)
+    email=models.EmailField(unique=True)
+    password=models.CharField(max_length=32)
+    profile_photo=models.ImageField(upload_to="profile_images/")
+
+    def __str__(self):
+        return self.first_name
+
+
+#http://localhost:8000/media/profile_images/photo.jpg
--- myproject/urls.py
+++ myproject/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+
+from django.conf import settings
+from django.conf.urls.static import static
 '''
 you can imagine like routers in nodejs
 '''
@@ -29,3 +32,6 @@ urlpatterns = [
     path('products/',include('myfirstapp.urls')),
     path("__reload__/", include("django_browser_reload.urls"))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
