5483e13fe88060d713a6e5193c54abb669a945c8
SaiAkshay456 <saiakshayorg@gmail.com>
2025-05-23 18:28:22 +0530

    learnt about one to many in django

--- myfirstapp/admin.py
+++ myfirstapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User,UserReview
 # Register your models here.
 
 admin.site.register(User)
+admin.site.register(UserReview)
--- myfirstapp/models.py
+++ myfirstapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.utils import timezone
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
 
@@ -10,9 +11,23 @@ class User(models.Model):
     password=models.CharField(max_length=32)
     profile_photo=models.ImageField(upload_to="profile_images/")
     description=models.TextField(default="")
+    date_to=models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.first_name
 
 
 #http://localhost:8000/media/profile_images/photo.jpg
+
+class UserReview(models.Model):
+
+    userr=models.ForeignKey(User,on_delete=models.CASCADE,related_name='user_review')
+    rating=models.IntegerField(validators=[
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ])
+    description=models.TextField(default="")
+    date_to=models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.userr.first_name} review for user'
